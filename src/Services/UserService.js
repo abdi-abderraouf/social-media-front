19fda724d48e9e136723d5b281d225e31bf772c7
abdi-abderraouf <abdiabderraouf@gmail.com>
2023-04-06 14:51:47 +0200

    second commit

--- src/Services/UserService.js
+++ src/Services/UserService.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 const UserService={}
 UserService.register=function(data){
-return axios.post('http://www.localhost:5000/users/signup',data)
+return axios.post('https://backendsocialmedia.onrender.com/users/signup',data)
+//axios.post('http://www.localhost:5000/users/signup',data)
 }
 UserService.login=function(data){
-    return axios.post('http://www.localhost:5000/users/signin',data)
+    return axios.post('https://backendsocialmedia.onrender.com/users/signin',data)
     }
 export default UserService;
